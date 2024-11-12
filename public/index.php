def081d6393c45720b2c92f8a5eb445fe9b378e4
xtrAnxz <xt4zzzzXudx@proton.me>
2024-11-12 18:55:10 +0100

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+echo 'Hello World!';
+?>
+<head>
+    <title></title>
+    <link rel="stylesheet" href="../static/css/style.css" type="text/css">
+</head>
+<?php require '../template/footer.php' ?>
+
---
+++ template/footer.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+    <div class="container">
+        <p>Copyright &copy My.Shop All Rights Reserved. | Contact Me: +000 00 00 000</p>
+
+    </div>
+</footer>
